e8d16f8f47d9d45e56d410504c08fc790d6e3d07
Geert Bevin <gbevin@uwyn.com>
2021-12-04 10:23:00 -0500

    Timeline and midi track drawing cleanup and performance tweaks

--- MIDI Tape Recorder Plugin/UI/MidiTrackView.mm
+++ MIDI Tape Recorder Plugin/UI/MidiTrackView.mm
@@ -25,34 +25,55 @@
     
     CGContextSaveGState(context);
 
-    CGContextSetFillColorWithColor(context, [UIColor colorNamed:@"Gray4"].CGColor);
+    CGColor* gray2_color = [UIColor colorNamed:@"Gray2"].CGColor;
+    CGColor* gray4_color = [UIColor colorNamed:@"Gray4"].CGColor;
+    CGColor* teal_color = [UIColor colorNamed:@"Teal"].CGColor;
+    
+    // fill the background
+    
+    CGContextSetFillColorWithColor(context, gray4_color);
     CGContextFillRect(context, self.bounds);
 
-    CGColor* gray2_color = [UIColor colorNamed:@"Gray2"].CGColor;
-    CGColor* teal_color = [UIColor colorNamed:@"Teal"].CGColor;
     CGFloat x_offset =  MAX(0.0, _tracks.contentOffset.x - 10.0);
+
+    // draw the vertical second bars
+    
+    CGContextBeginPath(context);
+    CGContextSetStrokeColorWithColor(context, gray2_color);
+    
     for (int x = x_offset; x < self.frame.size.width && x < x_offset + _tracks.frame.size.width; ++x) {
-        CGContextSetStrokeColorWithColor(context, gray2_color);
         if (x % PIXELS_PER_SECOND == 0) {
             CGContextMoveToPoint(context, x, 0.0);
             CGContextAddLineToPoint(context, x, self.frame.size.height);
         }
-        CGContextStrokePath(context);
-        
+    }
+    
+    CGContextStrokePath(context);
+    
+    // draw the data preview
+    
+    CGContextBeginPath(context);
+    CGContextSetStrokeColorWithColor(context, teal_color);
+
+    for (int x = x_offset; x < self.frame.size.width && x < x_offset + _tracks.frame.size.width; ++x) {
         NSData* preview = self.preview;
         if (preview != nil && x < preview.length) {
             uint8_t activity = ((uint8_t*)preview.bytes)[x];
             if (activity != 0) {
-                CGContextSetStrokeColorWithColor(context, teal_color);
                 CGContextMoveToPoint(context, x, self.frame.size.height);
+                
+                // normalize the preview event count
                 float v = MIN(((float)activity / MAX_PREVIEW_EVENTS), 1.f);
+                // increase the weight of the lower events counts so that they show up more easily
                 v = pow(v, 1.0/2.0);
+                
                 CGContextAddLineToPoint(context, x, self.frame.size.height - v * self.frame.size.height);
-                CGContextStrokePath(context);
             }
         }
     }
     
+    CGContextStrokePath(context);
+
     CGContextRestoreGState(context);
 }
 
--- MIDI Tape Recorder Plugin/UI/TimelineView.mm
+++ MIDI Tape Recorder Plugin/UI/TimelineView.mm
@@ -27,27 +27,37 @@
     CGContextSaveGState(context);
 
     CGColor* gray0_color = [UIColor colorNamed:@"Gray0"].CGColor;
+    CGContextSetStrokeColorWithColor(context, gray0_color);
+    
     CGFloat x_offset =  MAX(0.0, _tracks.contentOffset.x - 10.0);
+    
+    // draw vertical second bars and the shorter quarter dividers
+    CGContextBeginPath(context);
+
     for (int x = x_offset; x < self.frame.size.width && x < x_offset + _tracks.frame.size.width; ++x) {
         if (x % PIXELS_PER_SECOND == 0) {
-            CGContextSetStrokeColorWithColor(context, gray0_color);
             CGContextMoveToPoint(context, x, 0.0);
             CGContextAddLineToPoint(context, x, self.frame.size.height);
-            CGContextStrokePath(context);
+        }
+        else if (x % (PIXELS_PER_SECOND / 4) == 0) {
+            CGContextMoveToPoint(context, x, 2.0 * self.frame.size.height / 3.0);
+            CGContextAddLineToPoint(context, x, self.frame.size.height);
+        }
+    }
+    
+    CGContextStrokePath(context);
 
+    // draw the second numbers
+    
+    for (int x = x_offset; x < self.frame.size.width && x < x_offset + _tracks.frame.size.width; ++x) {
+        if (x % PIXELS_PER_SECOND == 0) {
             CGRect textRect = CGRectMake(x + 4, 1, PIXELS_PER_SECOND - 8, self.frame.size.height - 2);
             [[NSString stringWithFormat:@"%d", int(x / PIXELS_PER_SECOND) + 1] drawInRect:textRect
                                                                            withAttributes:@{NSFontAttributeName: [UIFont systemFontOfSize:9],
                                                                                             NSForegroundColorAttributeName: [UIColor lightGrayColor]}];
         }
-        else if (x % (PIXELS_PER_SECOND / 4) == 0) {
-            CGContextSetStrokeColorWithColor(context, gray0_color);
-            CGContextMoveToPoint(context, x, 2.0 * self.frame.size.height / 3.0);
-            CGContextAddLineToPoint(context, x, self.frame.size.height);
-            CGContextStrokePath(context);
-        }
     }
-    
+
     CGContextRestoreGState(context);
 }
 
